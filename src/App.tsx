56706f3d7d8794afe778500292ea0174413e6689
chicoumbetov <shyngysmaaan@gmail.com>
2025-03-31 20:22:50 +0200

    use created components

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import "./App.css";
+import { About } from "./components/About";
+import Home from "./components/Home";
 import { LoadingScreen } from "./components/LoadingScreen";
 import { MobileMenu } from "./components/MobileMenu";
 import { Navbar } from "./components/Navbar";
@@ -7,17 +9,28 @@ import { Navbar } from "./components/Navbar";
 function App() {
   const [isLoaded, setIsLoaded] = useState(false);
   const [menuOpen, setMenuOpen] = useState(false);
+  const [darkMode, setDarkMode] = useState(false);
 
   return (
     <>
-      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}{" "}
+      {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       <div
         className={`min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
-        <Navbar menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
-        <MobileMenu menuOpen={menuOpen} setMenuOpen={setMenuOpen} />
+        <Navbar
+          menuOpen={menuOpen}
+          darkMode={darkMode}
+          setDarkMode={setDarkMode}
+        />
+        <MobileMenu
+          darkMode={darkMode}
+          menuOpen={menuOpen}
+          setMenuOpen={setMenuOpen}
+        />
+        <Home darkMode={darkMode} setDarkMode={setDarkMode} />
+        <About />
       </div>
     </>
   );
