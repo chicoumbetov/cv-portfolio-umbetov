1517f7c4ae8c313e6ef4c8b65a8ef8c35b4d0b1e
chicoumbetov <shyngysmaaan@gmail.com>
2025-03-31 22:32:30 +0200

    clean

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -19,86 +19,10 @@ const Home = ({ darkMode }: HomeProps) => {
       <div className="intro-content display-table">
         <div className="table-cell">
           <h6 className="intro-title mb-1">Welcome to portfolio</h6>
-          <h6 className="intro-title mb-1">Shynggys UMBETOV</h6>
+          <h6 className="intro-title mb-10">Shynggys UMBETOV</h6>
 
           <div className={darkMode ? "dark" : "white"}>
-            <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
-              {/*
-        <section className="py-10">
-          <div>
-            <h3 className="text-3xl py-1 dark:text-white ">Portofolio</h3>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              Since the beginning of my journey as a freelance designer and
-              developer, I've done remote work for
-              <span className="text-teal-500"> agencies </span>
-              consulted for <span className="text-teal-500">startups </span>
-              and collaborated with talanted people to create digital products
-              for both business and consumer use.
-            </p>
-            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
-              I offer from a wide range of services, including brand design,
-              programming and teaching.
-            </p>
-          </div>
-          <div className="flex flex-col gap-10 py-10 lg:flex-row lg:flex-wrap">
-            <div className="basis-1/3 flex-1 ">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web1}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web2}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web3}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web4}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web5}
-              />
-            </div>
-            <div className="basis-1/3 flex-1">
-              <Image
-                className="rounded-lg object-cover"
-                width={"100%"}
-                height={"100%"}
-                layout="responsive"
-                src={web6}
-              />
-            </div>
-          </div>
-        </section>
-        */}
-            </main>
+            <main className=" bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40"></main>
           </div>
 
           <p className="intro-subtitle">
@@ -121,10 +45,19 @@ const Home = ({ darkMode }: HomeProps) => {
           <p className="pt-3">
             <a
               className={`px-4 py-2 primary_btn text-black`}
-              href="#work"
+              href="#projects"
               role="button"
             >
-              <span>Check portfolio</span>
+              <span>View projects</span>
+            </a>
+          </p>
+          <p className="pt-3">
+            <a
+              className={`px-4 py-2 primary_btn text-black`}
+              href="#contact"
+              role="button"
+            >
+              <span>Contact Me</span>
             </a>
           </p>
         </div>
