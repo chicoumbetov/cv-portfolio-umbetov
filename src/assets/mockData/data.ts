e32ecaa9aeb3714ca5214ce00017bd6bdb5a7c1e
chicoumbetov <shyngysmaaan@gmail.com>
2025-03-31 22:31:14 +0200

    clean

---
+++ src/assets/mockData/data.ts
@@ -0,0 +1,320 @@
+// import carousel from "../img/carousel/screencapture-localhost-3000-2021-01-06-23_01_05.png";
+
+// 1fit.app
+import stock1fit1 from "../img/1fitApp/1fit.app.png";
+import stock1fit2 from "../img/1fitApp/1fit.app2.png";
+import stock1fit3 from "../img/1fitApp/1fit.app3.png";
+
+// omedom
+import omedom3bankAcc from "../img/omedom/bank-account-connection.png";
+import omedom4budgetOption from "../img/omedom/budget-options-page.png";
+import omedom6Budget from "../img/omedom/budget-page.png";
+import omedom5createImmo from "../img/omedom/creation-immo-page.png";
+import omedom7immo from "../img/omedom/immobilier-page.png";
+import omedom8modifNotif from "../img/omedom/modif-notif-page.png";
+import omedom1 from "../img/omedom/mon-compte.png";
+import omedom9bankMouv from "../img/omedom/mouvement-bank-page.png";
+import omedom10Mouv from "../img/omedom/mouvement-page.png";
+import omedom11Mouv from "../img/omedom/parameter-doc-generation.png";
+import omedom12RecapWater from "../img/omedom/recap-water.png";
+import omedom2treso from "../img/omedom/tresory-page.png";
+// import omedom13quittanceDownloadPage from "../img/omedom/quittance-download-page.png";
+
+// scanflock
+import scanflock2 from "../img/scanflock/scanflock-mon-cabinet.png";
+import scanflock3 from "../img/scanflock/scanflock-telecons.png";
+
+// klubnest
+import klubScreenShot2 from "../img/klubnest/concept-klubnest.png";
+import klubScreenShot1 from "../img/klubnest/homepage-klubnest.png";
+import klubScreenShot4 from "../img/klubnest/presentation_klub.png";
+import klubScreenShot5 from "../img/klubnest/Profil-membre.png";
+import klubScreenShot6 from "../img/klubnest/Rejoignez-nous.png";
+import klubScreenShot3 from "../img/klubnest/tous-klub.png";
+
+// Minsk
+import minsk3 from "../img/minsk/5sformat.png";
+import minsk1 from "../img/minsk/mainpage.png";
+import minsk2 from "../img/minsk/page.png";
+import minsk4 from "../img/minsk/shoepage.png";
+import {
+  default as minsk5,
+  default as minsk6,
+  default as minsk7,
+} from "../img/simple-project.png";
+
+// yolla
+import yolla1 from "../img/yolla/About Us.png";
+import yolla2 from "../img/yolla/Bank Details.png";
+import yolla3 from "../img/yolla/Billing information.png";
+import yolla4 from "../img/yolla/Browse Vendor Directory.png";
+import yolla5 from "../img/yolla/Business details and profile setup.png";
+import yolla6 from "../img/yolla/Change password.png";
+import yolla7homepage from "../img/yolla/Home Page-1.png";
+import yolla8 from "../img/yolla/Log in.png";
+import yolla9 from "../img/yolla/Notification settings.png";
+import yolla10 from "../img/yolla/User Confirm Details.png";
+import yolla11 from "../img/yolla/User Dashboard - Request history.png";
+
+// wedding organiser`
+import wedding3 from "../img/wedding/wedding-sign-in.png";
+import wedding2 from "../img/wedding/wedding-signUP.png";
+import wedding1 from "../img/wedding/wedding1.png";
+
+// Pure javascript
+import pureJs2 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.44.46.png";
+import pureJs3 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.45.04.png";
+import pureJs4 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.45.53.png";
+import pureJs5 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.46.31.png";
+import pureJs6 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.46.38.png";
+import pureJs7 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.47.18.png";
+import pureJs8 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.47.28.png";
+import {
+  default as pureJs10,
+  default as pureJs9,
+} from "../img/pure-javascript/Screenshot 2022-01-09 at 19.47.37.png";
+import pureJs11 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.47.45.png";
+import pureJs12 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.48.33.png";
+import pureJs1 from "../img/pure-javascript/Screenshot 2022-01-09 at 19.50.09.png";
+
+// React Quiz
+import reactQuiz from "../img/react-quiz.png";
+import reactQuiz2 from "../img/react-quiz2.png";
+
+// Kanape selling
+import kanap1 from "../img/kanape/kanap1.png";
+import kanap2 from "../img/kanape/kanap2.png";
+import kanap3 from "../img/kanape/kanap3.png";
+
+// Oh my food
+import ohmyfood2 from "../img/ohmyfood/ohmyfood-mobile.png";
+import ohmyfood1 from "../img/ohmyfood/ohmyfood.png";
+import ohmyfood3 from "../img/ohmyfood/ohmyfood3.png";
+import ohmyfood4 from "../img/ohmyfood/ohmyfood4.png";
+
+// Reservia
+import reservia2 from "../img/reservia/reservia-hotel-mobile.png";
+import reservia1 from "../img/reservia/reservia-hotel.png";
+
+export const sliderItems = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/XsdmR2c/1.png",
+    title: "SUMMER SALE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "f5fafd",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/DG69bQ4/2.png",
+    title: "AUTUMN COLLECTION",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fcf1ed",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cXFnLLV/3.png",
+    title: "LOUNGEWEAR LOVE",
+    desc: "DON'T COMPROMISE ON STYLE! GET FLAT 30% OFF FOR NEW ARRIVALS.",
+    bg: "fbf0f4",
+  },
+];
+
+export const categories = [
+  {
+    id: 1,
+    img: stock1fit1,
+    title: "1fit.app",
+    projectLink: "https://1fit.app",
+    screenshots: [stock1fit1, stock1fit2, stock1fit3],
+    usedTechno: "React JS, Typescript, HTML5, CSS3, SASS",
+  },
+  {
+    id: 2,
+    img: omedom7immo,
+    title: "Omedom",
+    projectLink: "https://app.omedom.com",
+    githubLink: "https://github.com/chicoumbetov/ui_app",
+    screenshots: [
+      omedom1,
+      omedom2treso,
+      omedom3bankAcc,
+      omedom4budgetOption,
+      omedom5createImmo,
+      omedom6Budget,
+      omedom7immo,
+      omedom8modifNotif,
+      omedom9bankMouv,
+      omedom10Mouv,
+      omedom11Mouv,
+      omedom12RecapWater,
+    ],
+    usedTechno:
+      "React Native, Typescript, AWS cloud services: AppSync, Cognito, Dynamo DB",
+  },
+  {
+    id: 3,
+    img: scanflock2,
+    title: "Scanflock done, deployment in process",
+    // projectLink: "",
+    githubLink: "https://github.com/chicoumbetov/vetani_app",
+    screenshots: [, scanflock3],
+    usedTechno:
+      "React Native, Typescript, AWS cloud services: AppSync, Cognito, Dynamo DB",
+  },
+  {
+    id: 4,
+    img: klubScreenShot1,
+    title: "Klubnest",
+    projectLink: "https://dev.klubnest.fr/",
+    screenshots: [
+      klubScreenShot1,
+      klubScreenShot2,
+      klubScreenShot3,
+      klubScreenShot4,
+      klubScreenShot5,
+      klubScreenShot6,
+    ],
+    usedTechno:
+      "Javascript, PHP, MySQL, HTML5, CSS3, SASS, Bootstrap, LightSlier, Google maps API",
+  },
+  {
+    id: 5,
+    img: wedding2,
+    title: "Wedding organizer",
+    projectLink: "https://wedding-organize.netlify.app/",
+    githubLink: "https://github.com/chicoumbetov/wedding-organize",
+    backendLink: "https://wedding-react-js.herokuapp.com/posts",
+    usedTechno: "React, Redux, Express JS, Node JS, MongoDB, HTML5, CSS3",
+    screenshots: [wedding1, wedding3],
+  },
+  {
+    id: 6,
+    img: kanap1,
+    title: "E - commerce kanapé selling",
+    projectLink: "https://chicoumbetov.github.io/kanap-node-js/",
+    githubLink: "https://github.com/chicoumbetov/kanap-node-js",
+    backendLink: "https://kanap-project.herokuapp.com/api/products",
+    usedTechno: "Javascript, Express JS, Node JS, Mongo DB, HTML5, CSS3",
+    screenshots: [kanap2, kanap3],
+  },
+  {
+    id: 7,
+    img: yolla7homepage,
+    title: "yolla services, Kuala - Lumpur, Indonesia",
+    projectLink: "",
+    githubLink: "https://github.com/aiya94/yolla-app",
+    usedTechno: "React, Redux, Golang",
+    screenshots: [
+      yolla1,
+      yolla2,
+      yolla3,
+      yolla4,
+      yolla5,
+      yolla6,
+      yolla8,
+      yolla9,
+      yolla10,
+      yolla11,
+    ],
+  },
+  {
+    id: 8,
+    img: minsk1,
+    title: "Belorussian e-commerce shop",
+    projectLink: "https://chicoumbetov.github.io/minsk",
+    screenshots: [minsk2, minsk3, minsk4, minsk5, minsk6, minsk7],
+    usedTechno: "React js, HTML5, CSS3",
+  },
+
+  {
+    id: 10,
+    img: pureJs2,
+    title: "Pure javascript mini projects collection",
+    projectLink: "https://chicoumbetov.github.io/vladilen-gallery/",
+    githubLink: "https://github.com/chicoumbetov/vladilen-gallery",
+    screenshots: [
+      pureJs1,
+      pureJs3,
+      pureJs4,
+      pureJs5,
+      pureJs6,
+      pureJs7,
+      pureJs8,
+      pureJs9,
+      pureJs10,
+      pureJs11,
+      pureJs12,
+    ],
+  },
+  {
+    id: 11,
+    img: "https://images.pexels.com/photos/5480696/pexels-photo-5480696.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500",
+    title: "Buyers jungle in process",
+    // projectLink: "",
+    screenshots: [],
+  },
+  {
+    id: 12,
+    img: ohmyfood1,
+    title: "Oh my food restaurant",
+    projectLink: "https://github.com/chicoumbetov/umbetov_shynggys_3_04072021",
+    screenshots: [ohmyfood2, ohmyfood3, ohmyfood4],
+  },
+  {
+    id: 13,
+    img: reactQuiz,
+    title: "Typescript Quiz app. Fetch data from opentdb.com(Trivia database)",
+    projectLink: "https://chicoumbetov.github.io/react-typescript-quiz",
+    screenshots: [reactQuiz2],
+  },
+  {
+    id: 14,
+    img: reservia1,
+    title: "Reservia hotels reservation app",
+    projectLink: "https://chicoumbetov.github.io/umbetov_shynggys_2_18042021/",
+    screenshots: [reservia2],
+  },
+  {
+    id: 15,
+    img: "https://images.pexels.com/photos/2983464/pexels-photo-2983464.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    title: "FullStack Resto Angular js App in process",
+    githubLink: "https://github.com/chicoumbetov/resto_angular",
+    screenshots: [],
+  },
+];
+
+export const popularProducts = [
+  {
+    id: 1,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Mens-Jake-Guitar-Vintage-Crusher-Tee_68382_1_lg.png",
+  },
+  {
+    id: 2,
+    img: "https://cdn.shopify.com/s/files/1/0101/4832/products/Angela_Natural_Tee.png?v=1606780388",
+  },
+  {
+    id: 3,
+    img: "https://www.prada.com/content/dam/pradanux_products/U/UCS/UCS319/1YOTF010O/UCS319_1YOT_F010O_S_182_SLF.png",
+  },
+  {
+    id: 4,
+    img: "https://www.burdastyle.com/pub/media/catalog/product/cache/7bd3727382ce0a860b68816435d76e26/107/BUS-PAT-BURTE-1320516/1170x1470_BS_2016_05_132_front.png",
+  },
+  {
+    id: 5,
+    img: "https://images.ctfassets.net/5gvckmvm9289/3BlDoZxSSjqAvv1jBJP7TH/65f9a95484117730ace42abf64e89572/Noissue-x-Creatsy-Tote-Bag-Mockup-Bundle-_4_-2.png",
+  },
+  {
+    id: 6,
+    img: "https://d3o2e4jr3mxnm3.cloudfront.net/Rocket-Vintage-Chill-Cap_66374_1_lg.png",
+  },
+  {
+    id: 7,
+    img: "https://www.vintageindustries.nl/download_front/qympzk1762/2217_Arrow_Jacket_Forest.png",
+  },
+  {
+    id: 8,
+    img: "https://www.pngarts.com/files/3/Women-Jacket-PNG-High-Quality-Image.png",
+  },
+];
