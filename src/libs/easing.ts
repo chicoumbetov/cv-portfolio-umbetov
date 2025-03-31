19b16119a3e2f776be6f84c225a8d26c811e3353
chicoumbetov <shyngysmaaan@gmail.com>
2025-03-31 17:47:04 +0200

    easing funcs ts

---
+++ src/libs/easing.ts
@@ -0,0 +1,28 @@
+type EasingFunction = (t: number, b: number, c: number, d: number) => number;
+
+const easing: Record<string, EasingFunction> = {
+  easeInQuad: (t, b, c, d) => c * (t /= d) * t + b,
+  easeOutQuad: (t, b, c, d) => -c * (t /= d) * (t - 2) + b,
+  easeInOutQuad: (t, b, c, d) =>
+    (t /= d / 2) < 1 ? (c / 2) * t * t + b : (-c / 2) * (--t * (t - 2) - 1) + b,
+  easeInCubic: (t, b, c, d) => c * (t /= d) * t * t + b,
+  easeOutCubic: (t, b, c, d) => c * ((t = t / d - 1) * t * t + 1) + b,
+  easeInOutCubic: (t, b, c, d) =>
+    (t /= d / 2) < 1
+      ? (c / 2) * t * t * t + b
+      : (c / 2) * ((t -= 2) * t * t + 2) + b,
+  easeInQuart: (t, b, c, d) => c * (t /= d) * t * t * t + b,
+  easeOutQuart: (t, b, c, d) => -c * ((t = t / d - 1) * t * t * t - 1) + b,
+  easeInOutQuart: (t, b, c, d) =>
+    (t /= d / 2) < 1
+      ? (c / 2) * t * t * t * t + b
+      : (-c / 2) * ((t -= 2) * t * t * t - 2) + b,
+  easeInQuint: (t, b, c, d) => c * (t /= d) * t * t * t * t + b,
+  easeOutQuint: (t, b, c, d) => c * ((t = t / d - 1) * t * t * t * t + 1) + b,
+  easeInOutQuint: (t, b, c, d) =>
+    (t /= d / 2) < 1
+      ? (c / 2) * t * t * t * t * t + b
+      : (c / 2) * ((t -= 2) * t * t * t * t + 2) + b,
+};
+
+export default easing;
