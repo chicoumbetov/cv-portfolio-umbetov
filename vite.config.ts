2ea326afe1120a34d9faa654d92ba15f82e5fb57
chicoumbetov <shyngysmaaan@gmail.com>
2025-03-31 22:31:07 +0200

    add slash

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import { defineConfig } from "vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "cv-portfolio-umbetov",
+  base: "/cv-portfolio-umbetov",
 });
