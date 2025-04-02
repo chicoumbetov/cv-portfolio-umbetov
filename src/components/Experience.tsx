265d95a6d61439b63fd40a5a942cb93bc24e4ff5
chicoumbetov <shyngysmaaan@gmail.com>
2025-04-02 23:31:24 +0200

    refactor experience

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -21,20 +21,34 @@ export const Experience = ({ darkMode }: ExperienceProps) => {
             <div className="space-y-4 text-gray-300">
               <div>
                 <h4 className="font-semibold">
-                  Software Engineer at ABC Corp (2020 - Present)
+                  Trace Software International (Group Trace) (2023 - Present)
                 </h4>
-                <p>
-                  Developed and maintained microservices for cloud-based
-                  applications.
-                </p>
+                <h5 className="font-bold">Developer</h5>
+                <div className="space-y-1 text-gray-200">
+                  <p>
+                    Created from scratch electrical schema builder applications.
+                  </p>
+                  <p>
+                    Developed and maintained microservices for cloud-based
+                    applications.
+                  </p>
+                  <p>Realised integration of Graph QL APIs</p>
+                </div>
               </div>
 
               <div>
-                <h4 className="font-semibold">Intern at DEF Startups (2019)</h4>
-                <p>
-                  Assisted in building front-end components and integration REST
-                  APIs
-                </p>
+                <h4 className="font-semibold">
+                  BIM&CO (Group Trace) (2022-2023)
+                </h4>
+                <h5>Developer</h5>
+                <p>Integration REST APIs</p>
+              </div>
+
+              <div>
+                <h4 className="font-semibold">
+                  Developer at Global Climate Initiatives (2022)
+                </h4>
+                <p>Integration REST APIs</p>
               </div>
             </div>
           </div>
