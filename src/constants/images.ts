1e90cfaf40b5f7ae7ee9c39126d7ef4d699c164c
chicoumbetov <shyngysmaaan@gmail.com>
2025-04-04 21:55:06 +0200

    images constants

---
+++ src/constants/images.ts
@@ -0,0 +1,72 @@
+import api from "../assets/tech-icons/api.png";
+import cpp from "../assets/tech-icons/cpp.png";
+import css from "../assets/tech-icons/css.png";
+import email from "../assets/tech-icons/email.png";
+import figma from "../assets/tech-icons/figma.png";
+import flutter from "../assets/tech-icons/flutter.png";
+import git from "../assets/tech-icons/git.png";
+import graphql from "../assets/tech-icons/graphql.png";
+import html from "../assets/tech-icons/html.png";
+import javascript from "../assets/tech-icons/javascript.png";
+import mobile from "../assets/tech-icons/mobile.png";
+import mu5 from "../assets/tech-icons/mu5.png";
+import node from "../assets/tech-icons/node.png";
+import python from "../assets/tech-icons/python.png";
+import react from "../assets/tech-icons/react.png";
+import redux from "../assets/tech-icons/redux.png";
+import sass from "../assets/tech-icons/sass.png";
+import typescript from "../assets/tech-icons/typescript.png";
+import vue from "../assets/tech-icons/vue.png";
+
+import about01 from "../assets/tech-icons/about01.png";
+import about02 from "../assets/tech-icons/about02.png";
+import about03 from "../assets/tech-icons/about03.png";
+import about04 from "../assets/tech-icons/about04.png";
+
+import circle from "../assets/tech-icons/circle.svg";
+import logo from "../assets/tech-icons/logo.png";
+import profile from "../assets/tech-icons/profile.png";
+
+import adidas from "../assets/tech-icons/adidas.png";
+import amazon from "../assets/tech-icons/amazon.png";
+import asus from "../assets/tech-icons/asus.png";
+import bolt from "../assets/tech-icons/bolt.png";
+import nb from "../assets/tech-icons/nb.png";
+import skype from "../assets/tech-icons/skype.png";
+import spotify from "../assets/tech-icons/spotify.png";
+
+export default {
+  email,
+  mobile,
+  api,
+  cpp,
+  css,
+  figma,
+  flutter,
+  git,
+  graphql,
+  html,
+  javascript,
+  mu5,
+  node,
+  python,
+  react,
+  redux,
+  sass,
+  typescript,
+  vue,
+  about01,
+  about02,
+  about03,
+  about04,
+  profile,
+  circle,
+  logo,
+  adidas,
+  amazon,
+  asus,
+  bolt,
+  nb,
+  skype,
+  spotify,
+};
