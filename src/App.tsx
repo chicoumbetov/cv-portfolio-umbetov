df1f203cc77c4aa7b9ae52d123b9432414f5f9a7
chicoumbetov <shyngysmaaan@gmail.com>
2025-04-04 21:55:40 +0200

    App imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,16 @@
 import { useState } from "react";
 
-import BackToTop from "./components/BackToTop";
-import { Contact } from "./components/Contact";
-import { Education } from "./components/Education";
 import { Experience } from "./components/Experience";
-import Home from "./components/Home";
+
 import { LoadingScreen } from "./components/LoadingScreen";
-import { MobileMenu } from "./components/MobileMenu";
 import { Navbar } from "./components/Navbar";
-import Projects from "./components/Projects";
-import { Skills } from "./components/Skills";
 
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import "./App.css";
+import { BackToTop, Education, Home, Projects } from "./components";
 import Certificates from "./components/Certificates";
+import { Footer, Skills as Skills2 } from "./container";
 import i18nRoot, { i18nextLng } from "./i18n/config";
 import { Language } from "./types";
 
@@ -37,10 +33,12 @@ function App() {
     <>
       {!isLoaded && <LoadingScreen onComplete={() => setIsLoaded(true)} />}
       <div
-        className={`min-h-screen transition-opacity duration-700 ${
+        className={`app min-h-screen transition-opacity duration-700 ${
           isLoaded ? "opacity-100" : "opacity-0"
         } bg-black text-gray-100`}
       >
+        {/*<Contact />*/}
+
         <Navbar
           menuOpen={menuOpen}
           setMenuOpen={setMenuOpen}
@@ -49,19 +47,15 @@ function App() {
           language={language}
           handleLanguageChange={handleLanguageChange}
         />
-        <MobileMenu
-          darkMode={darkMode}
-          menuOpen={menuOpen}
-          setMenuOpen={setMenuOpen}
-        />
         <Home darkMode={darkMode} setDarkMode={setDarkMode} />
-        <Skills darkMode={darkMode} />
+        {/*<Skills darkMode={darkMode} />*/}
+        <Skills2 darkMode={darkMode} />
         <Experience darkMode={darkMode} />
         <Projects darkMode={darkMode} />
         <Education darkMode={darkMode} />
         <BackToTop darkMode={darkMode} />
         <Certificates darkMode={darkMode} />
-        <Contact />
+        <Footer darkMode={darkMode} />
       </div>
     </>
   );
