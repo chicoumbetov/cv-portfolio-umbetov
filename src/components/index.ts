3a3f939d76ecb961104b610dffc3c11e03a00223
chicoumbetov <shyngysmaaan@gmail.com>
2025-04-04 21:55:21 +0200

    index components

---
+++ src/components/index.ts
@@ -0,0 +1,6 @@
+import BackToTop from "./BackToTop";
+import Education from "./Education";
+import Home from "./Home";
+import Projects from "./Projects";
+
+export { BackToTop, Education, Home, Projects };
